651c5802167fd7c462834ba7882aa5ef2b983095
Adam Coddington <me@adamcoddington.net>
2021-08-19 21:30:05 -0700

    Reformatting with black

--- setup.py
+++ setup.py
@@ -15,65 +15,67 @@ from setuptools import setup
 
 def read(*names, **kwargs):
     with io.open(
-        join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
+        join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")
     ) as fh:
         return fh.read()
 
 
 setup(
-    name='boox-annotations-to-anki',
-    version='0.1.1',
-    license='MIT',
-    description='Create cards in Anki from your Onyx Boox annotations.',
-    long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    name="boox-annotations-to-anki",
+    version="0.1.1",
+    license="MIT",
+    description="Create cards in Anki from your Onyx Boox annotations.",
+    long_description="%s\n%s"
+    % (
+        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub(
+            "", read("README.rst")
+        ),
+        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
     ),
-    author='Adam Coddington',
-    author_email='me@adamcoddington.net',
-    url='https://github.com/coddingtonbear/boox-annotations-to-anki',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    author="Adam Coddington",
+    author_email="me@adamcoddington.net",
+    url="https://github.com/coddingtonbear/boox-annotations-to-anki",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         # uncomment if you test on these interpreters:
         # 'Programming Language :: Python :: Implementation :: IronPython',
         # 'Programming Language :: Python :: Implementation :: Jython',
         # 'Programming Language :: Python :: Implementation :: Stackless',
-        'Topic :: Utilities',
+        "Topic :: Utilities",
     ],
     project_urls={
-        'Changelog': 'https://github.com/coddingtonbear/boox-annotations-to-anki/blob/master/CHANGELOG.rst',
-        'Issue Tracker': 'https://github.com/coddingtonbear/boox-annotations-to-anki/issues',
+        "Changelog": "https://github.com/coddingtonbear/boox-annotations-to-anki/blob/master/CHANGELOG.rst",
+        "Issue Tracker": "https://github.com/coddingtonbear/boox-annotations-to-anki/issues",
     },
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=[
-        'requests>=2.26.0,<3.0',
-        'boox-annotation-parser>=0.1,<1.0',
-        'rich>=10.7.0,<11.0',
-        'appdirs>=1.4.4,<2.0',
+        "requests>=2.26.0,<3.0",
+        "boox-annotation-parser>=0.1,<1.0",
+        "rich>=10.7.0,<11.0",
+        "appdirs>=1.4.4,<2.0",
     ],
     extras_require={
         # eg:
@@ -81,8 +83,8 @@ setup(
         #   ':python_version=="2.6"': ['argparse'],
     },
     entry_points={
-        'console_scripts': [
-            'boox-annotations-to-anki = boox_annotations_to_anki.cli:main',
+        "console_scripts": [
+            "boox-annotations-to-anki = boox_annotations_to_anki.cli:main",
         ]
     },
 )
